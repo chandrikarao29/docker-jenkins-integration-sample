fc76d6ea2afd6b6b21e81fda59abf94fcdd3b8e3
chandrika <chandrikarao29@gmail.com>
2021-02-14 01:06:44 -0500

    changed test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -10,7 +10,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class DemoApplicationTests {
-	public String baseUrl = "http://www.google.com/";
 
 
 	@Test
