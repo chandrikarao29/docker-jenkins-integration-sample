f61bf2f75a6269f5f71d7eb950a449aa859b6a88
chandrika <chandrikarao29@gmail.com>
2021-02-14 00:10:25 -0500

    added selenium

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,13 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
+	public String baseUrl = "http://www.google.com/";
 
 
 	@Test
