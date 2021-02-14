71077ff9b0bed90de473de0b9a368aedff8758b7
chandrika <chandrikarao29@gmail.com>
2021-02-14 01:20:13 -0500

    smoke test

--- src/test/java/com/example/demo/SmokeTest.java
+++ src/test/java/com/example/demo/SmokeTest.java
@@ -1,23 +1,30 @@
 package com.example.demo;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class DemoApplicationTests {
+public class SmokeTest {
 
+	@Autowired
+	private HelloController controller;
 
 	@Test
 	public void contextLoads() {
+		assertThat(controller).isNotNull();
+
 	}
 
 	@Test
 	public void testCase() {
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\chand\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\chand\\Downloads\\chromedriver_win32\\chromedriver.exe");
 
 		WebDriver driver = new ChromeDriver();
 		driver.navigate().to("http://localhost:8080/demo");
