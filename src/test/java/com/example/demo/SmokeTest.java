1ba1f46edc96426a0cfb6164cc83eabd34b9af12
chandrika <chandrikarao29@gmail.com>
2021-02-15 16:17:11 -0500

    changed webdriver

--- src/test/java/com/example/demo/SmokeTest.java
+++ src/test/java/com/example/demo/SmokeTest.java
@@ -1,8 +1,8 @@
 package com.example.demo;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -14,6 +14,7 @@ public class SmokeTest {
 
 	@Autowired
 	private HelloController controller;
+	private static WebDriver driver;
 
 	@Test
 	public void contextLoads() {
@@ -26,19 +27,26 @@ public class SmokeTest {
 		System.setProperty("webdriver.chrome.driver",
 				"C:\\Users\\chand\\Downloads\\chromedriver_win32\\chromedriver.exe");
 
-		WebDriver driver = new ChromeDriver();
-		//driver.navigate().to("http://localhost:8080/demo");
-		//String title = driver.getTitle();
-		//assertEquals(title, "demo");
+		driver = new ChromeDriver();
+		// driver.navigate().to("http://localhost:8080/demo");
+		// String title = driver.getTitle();
+		// assertEquals(title, "demo");
 	}
 
 	@Test
 	public void testCase2() {
 
 	}
-	
+
 	@Test
 	public void testCase3() {
 
 	}
+
+	@AfterAll
+    public static void cleanUp(){
+        if (driver != null) {
+            driver.close();
+            driver.quit();
+        }
 }
